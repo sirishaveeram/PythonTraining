28d1b0c80e2be8ea5a81baa5081c89b8368f4e37
sirishaveeram <91636368+sirishaveeram@users.noreply.github.com>
2021-10-08 11:04:28 -0400

    Update task2.py
    This is my second task file

--- task2.py
+++ task2.py
@@ -1,1 +1,23 @@
+(Q1.) Write a program in Python to perform the following operation:
+If a number is divisible by 3 it should print “Consultadd” as a string
+x = int(input("Enter a value:"))
+if x % 3==0:
+    print("Consultadd")
+else:
+    print("value is not divisable by 3")
+If a number is divisible by 5 it should print “Python Training” as a string
+x = int(input("Enter a value:"))
+if x % 5==0:
 
+    print("Python Training")
+else:
+    print("value is not divisable by 5")
+
+If a number is divisible by both 3 and 5 it should print “Consultadd - Python Training” as a string.
+x = int(input("Enter a value:"))
+if x % 3==0  and x % 5==0:
+
+    print("“Consultadd - Python Training”")
+else:
+    print("value is not divisable by 3 and 5")
+    
