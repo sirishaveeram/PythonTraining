01bf94e67e7c91bf7065f532c2d859e0f6ffbaa1
sirishaveeram <91636368+sirishaveeram@users.noreply.github.com>
2021-10-10 13:45:06 -0400

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,1 +1,10 @@
-
+(Q1.) Create a list of 10 elements of four different data types like int, string, complex and float.
+(ANSWERS:)
+l = [1, "siri", 1 + 2j, 4.5,1,"siri",2.6,4,2+3j,10]
+(Q2.)Create a list of size 5 and execute the slicing structure
+(ANSWER)
+p = [1, "siri", 1 + 2j, 4.5, 1]
+print(p[0:3])
+(output:)[1, 'siri', (1+2j)]
+(Q3.) Write a program to get the sum and multiply of all the items in a given list.
+(ANSWER:)
